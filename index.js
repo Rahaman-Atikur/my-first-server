89170c3093932ed65a000ddae47f01c6503b40e4
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-30 22:50:11 +0000

    Phone is running

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('My phone information is coming soon');
 })
-
-app.get('/data', (res, req) => {
-    res.send("More data is coming soon");
-})
-
 
 app.listen(port, () => {
-    console.log(`My first server is running on port:${port}`)
+    console.log(`My phone server is running on port:${port}`);
 })
