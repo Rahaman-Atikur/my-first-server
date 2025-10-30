cdbfd90cd4de43044a1b286baa9e1a865c555674
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-30 23:23:56 +0000

    Done

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('My phone information is coming soon');
+    res.send('My phone information is not  coming soon');
+});
+
+app.get('/phones', (req, res) => {
+    res.send(phones);
+})
+
+app.get('/phones/:id', (res, req) => {
+    const id = parseInt(req.params.id);
+    console.log("I need the params id:", id);
+    const phone = phones.find(phone => phone.id == id) || {};
 })
 
 app.listen(port, () => {
